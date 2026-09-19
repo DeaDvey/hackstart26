8eab58d7169caf0fe3464811ebe3d2d0b0ae795b
deadvey <deadvey@deadvey.com>
2026-09-19 13:35:57 +0100

    klaxon

--- main.py
+++ main.py
@@ -19,6 +19,17 @@ def page3(email1=None, email2=None):
 	email1 = request.form['email1']
 	email2 = request.form['email2']
 	if email1 != email2:
-		return render_template('error.html', error="Your emails do not match")
+		return render_template('error.html', error="Your emails do not match.")
 	else:
 		return render_template('form3.html', email=email1)
+
+@app.route('/page4', methods=['POST'])
+def page4(email_original=None, email_backwards=None):
+	# These should be the same and should both be recieved in reversed format
+	email_original = request.form['email_original']
+	email_backwards = request.form['email_backwards']
+	actual_email = email_original[::-1]
+	if email_original != email_backwards:
+		return render_template('error.html', error="Your reversed emails do not match.")
+	else:
+		return render_template('form4.html', email=actual_email)
