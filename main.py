d239b0a18303e6213bfb13e4b49c1c749ca5258d
deadvey <deadvey@deadvey.com>
2026-09-19 11:18:28 +0100

    Added dynamic and static resources examples

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import request
+from flask import render_template
+from flask import url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
 	return "<p>Hello, World!</p>"
+
+@app.route('/dynamic/')
+@app.route('/dynamic/<name>')
+def dynamic(name=None):
+	return render_template('dynamic.html', person=name)
