a99aff901a1cc394d1d80141aae26b6d2df8608f
deadvey <deadvey@deadvey.com>
2026-09-19 12:58:11 +0100

    form2 mostly done

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ def page2(email=None):
 	return render_template('form2.html', email=email)
 
 @app.route('/page3', methods=['POST'])
-def page3(email=None):
-	email = request.form['email']
-	return render_template('form3.html', email=email)
+def page3(email1=None, email2=None):
+	email1 = request.form['email1']
+	email2 = request.form['email2']
+	if email1 != email2:
+		return render_template('error.html', error="Your emails do not match")
+	else:
+		return render_template('form3.html', email=email1)
